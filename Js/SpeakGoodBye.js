2469bb5e43532baa145d533d341de931ccab4653
maansik9824 <69757117+maansik9824@users.noreply.github.com>
2020-08-24 22:13:19 +0530

    Add files via upload

---
+++ Js/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(window){
+
+    var byespeaker={};
+    var speakword="Good Bye";
+    byespeaker.speak= function(name){
+       console.log(speakword+" "+name);
+    };
+    window.byespeaker=byespeaker;
+})(window);
+
+
